189fabded532d880e855fbf497f50dd12f536181
BrunoHRF <BrunoHRF@outlook.com>
2019-06-19 23:06:21 -0300

    Adicionando script, porem ainda sem conseguir tirar do DB

---
+++ js/buscarDados.js
@@ -0,0 +1,37 @@
+function init() {
+    var y = document.getElementById("getimage");
+    y.addEventListener('change', loadimage, false);
+
+     $("#form-contato").submit(function() {
+         let campoNome = $("#inputNome").val();
+         let campoIdade = $("#inputIdade").val();
+         let campoCidade = $("#inputCidade").val();
+         let data = new Date().toString();
+         let campoImg = document.getElementById('jsonstore').innerHTML;
+         
+         let contato = {
+             "inputNome": campoNome,
+             "inputIdade": campoIdade,
+             "inputCidade": campoCidade,
+             "DataAtual": data, 
+             "jsonstore": campoImg,
+         }
+         insertContato(contato);
+     });
+ }
+
+ function imageHandler(e2) {
+     var store = document.getElementById('imgstore');
+     store.innerHTML = '<img src="' + e2.target.result + '">';
+
+     var jsonstore = document.getElementById('jsonstore');
+     jsonstore.innerHTML = `{ "img": "${e2.target.result}" }`;
+
+ }
+
+ function loadimage(e1) {
+     var filename = e1.target.files[0];
+     var fr = new FileReader();
+     fr.onload = imageHandler;
+     fr.readAsDataURL(filename);
+ }
---
+++ js/script.js
@@ -0,0 +1,54 @@
+function get_db(){
+    let blog_db = JSON.parse(localStorage.getItem('db'));
+    if (!blog_db) {
+      blog_db = db;
+    };
+    return blog_db;
+  }
+function exibeContatos() {
+    blog_db = get_db();
+    $("#grid-main-user").html("");
+    var i;
+    for (i = 0; i < blog_db.data.length; i++) {
+    contato = blog_db.data[i];
+    $("#grid-main-user").append(`
+    <div class="w3-container">
+        <div class="w3-card-4 onclick="goToProfile(${contato.id}" style="width:70%">
+            <header class="w3-container w3-light-grey">
+            <h3>${contato.nome}</h3>
+            </header>
+            <div class="w3-container">
+            <p>${contato.data}</p>
+            <hr>
+            <p></p><br>
+            </div>
+            <button class="w3-buttonw3-teal">Like</button>
+        </div>
+    </div>
+    `   );
+    }
+}
+function goToProfile(id){
+    location.href = `user.html?id=${id}`;
+}
+function build_profile_page(){
+    urlParams = new URLSearchParams(window.location.search);
+    id = urlParams.get('id');
+    blog_db = get_db();
+    user = blog_db.data[id-1];
+    $("#grid-main-user").append(`
+    <div class="w3-container">
+        <div class="w3-card-4 onclick="goToProfile(${user.id}" style="width:70%">
+            <header class="w3-container w3-light-grey">
+            <h3>${user.nome}</h3>
+            </header>
+            <div class="w3-container">
+            <p>${user.data}</p>
+            <hr>
+            <p></p><br>
+            </div>
+            <button class="w3-buttonw3-teal">Like</button>
+        </div>
+    </div>
+    `   );
+}
